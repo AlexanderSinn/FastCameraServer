0eecf4d084e99a0059bcc3b16e3c61e29ad94905
Alexander Sinn <alexander.sinn@desy.de>
2026-01-20 14:35:28 +0100

    Add moment calculation using omp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@
 #include <m3api/xiApi.h> // Linux, OSX
 #endif
 
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
 #include "AllCameraParams.H"
 
 #include <memory.h>
@@ -37,34 +41,43 @@
 	} \
 
 
+template<class T>
+std::ostream& operator<< (std::ostream& os, std::vector<T> const& vec) {
+	os << '[';
+	for (unsigned long long i = 0; i < vec.size(); ++i) {
+		if (i != 0) {
+			os << ", ";
+		}
+		os << vec[i];
+	}
+	os << ']';
+	return os;
+}
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+	// initialize omp
+#ifdef _OPENMP
+	omp_set_num_threads(10);
+#pragma omp parallel
+	{
+#pragma omp critical
+		printf("Initialized omp thread %d\n", omp_get_thread_num());
+	}
+#endif
+
 	// image buffer
 	XI_IMG image;
 	memset(&image, 0, sizeof(image));
-	image.size = sizeof(XI_IMG);
+	image.size = SIZE_XI_IMG_V2;
 
 	HANDLE xiH = NULL;
 
 	// Retrieving a handle to the camera device 
 	printf("Opening first camera...\n");
     SAVECALL(xiOpenDevice(0, &xiH);)
-    
-	char device_sn[256] = {};
-	SAVECALL(xiGetDeviceInfoString(0, XI_PRM_DEVICE_SN, device_sn, 256));
-	char device_ip[512] = {};
-	SAVECALL(xiGetDeviceInfoString(0, XI_PRM_DEVICE_INSTANCE_PATH, device_ip, 512));
-	char device_t[256] = {};
-	SAVECALL(xiGetDeviceInfoString(0, XI_PRM_DEVICE_TYPE, device_t, 256));
-	char device_n[256] = {};
-	SAVECALL(xiGetDeviceInfoString(0, XI_PRM_DEVICE_NAME, device_n, 256));
 
-	std::cout
-		<< "XI_PRM_DEVICE_SN " << device_sn << '\n'
-		<< "XI_PRM_DEVICE_INSTANCE_PATH " << device_ip << '\n'
-		<< "XI_PRM_DEVICE_TYPE " << device_t << '\n'
-		<< "XI_PRM_DEVICE_NAME " << device_n << '\n'
-		<< std::endl;
     
 	for (auto& [pname, tname] : all_params) {
 		char result[512] = {};
@@ -74,11 +87,11 @@ int _tmain(int argc, _TCHAR* argv[])
 
 
 	// Setting "exposure" parameter (10ms=10000us)
-	SAVECALL(xiSetParamInt(xiH, XI_PRM_EXPOSURE, 50));
+	SAVECALL(xiSetParamInt(xiH, XI_PRM_EXPOSURE, 5));
 
 
-	const int im_width = 256;
-	const int im_height = 256;
+	const int im_width = 608;
+	const int im_height = 608;
 
 	SAVECALL(xiSetParamInt(xiH, XI_PRM_WIDTH, im_width));
 	SAVECALL(xiSetParamInt(xiH, XI_PRM_HEIGHT, im_height));
@@ -95,51 +108,59 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	SAVECALL(xiGetImage(xiH, 5000, &image));
 
-	auto start_time = std::chrono::steady_clock::now();
+	auto last = std::chrono::steady_clock::now();
 
-	auto last = start_time;
+	constexpr int expected_images = 100000;
 
+	long long last_image_num = 0;
+	long long dropped_frames = 0;
 
-	constexpr int expected_images = 1000000000;
+	std::vector<long long> time_hist(5000, 0);
+	std::vector<long long> px_h_hist(image.height, 0);
+	std::vector<long long> px_w_hist(image.width, 0);
 
-	//std::vector<double> delta_time_vec(100, 0.);
+	constexpr bool do_moments = true;
 
 	for (int images = 0; images < expected_images; images++)
 	{
-		// getting image from camera
 		SAVECALL(xiGetImage(xiH, 5000, &image));
-		//unsigned char pixel = *(unsigned char*)image.bp;
+
+		if constexpr (do_moments) {
+			long long px_sum = 0;
+			long long px_h_sum = 0;
+			long long px_w_sum = 0;
+
+#pragma omp parallel for reduction(+:px_sum,px_w_sum,px_h_sum)
+			for (int j = 0; j < im_height; ++j) {
+				for (int i = 0; i < im_width; ++i) {
+					long long value = ((unsigned char*)image.bp)[i + j * im_width];
+					px_sum += value;
+					px_w_sum += value * i;
+					px_h_sum += value * j;
+				}
+			}
+			px_h_hist[px_h_sum / px_sum] += 1;
+			px_w_hist[px_w_sum / px_sum] += 1;
+		}
 
 		auto current_time = std::chrono::steady_clock::now();
-
-		auto duration = std::chrono::duration<double, std::micro>(current_time - start_time);
 		auto delta = std::chrono::duration<double, std::micro>(current_time - last);
-
 		last = current_time;
 
-		unsigned char min_px = -1;
-		unsigned char max_px = 1;
+		time_hist[std::max(std::min(static_cast<int>(delta.count()), static_cast<int>(time_hist.size()-1)), 0)] += 1;
 
-		for (int j = 0; j < (im_width * im_height); ++j) {
-			min_px = std::min(min_px, ((unsigned char*)image.bp)[j]);
-			max_px = std::max(max_px, ((unsigned char*)image.bp)[j]);
+		if (images > 0) {
+			dropped_frames += std::max<long long>((image.nframe - last_image_num - 1), 0);
 		}
+		last_image_num = image.nframe;
 
-		//std::cout << delta.count() << " us\n";
-		if (images % 1 == 0) {
-			printf("%f us, min %d, max %d\n", delta.count(), int{ min_px }, int{ max_px });
-		}
-
-		//delta_time_vec[images] = delta.count();
-
-		//printf("Image %d (%dx%d) received from camera. First pixel value: %d; time: %f us delta: %f us\n",
-		//	images, (int)image.width, (int)image.height, pixel, duration.count(), delta.count());
+		//printf("%f us, ptr %p nframe %d, tsSec %d, tsUSec %d  \n", delta.count(), image.bp, image.nframe, image.tsSec, image.tsUSec);
 	}
 
-	//for (int images = 0; images < expected_images; images++) {
-
-	//	std::cout << delta_time_vec[images] << " us"  << std::endl;
-	//}
+	std::cout << "time_hist = " << time_hist << std::endl;
+	std::cout << "px_h_hist = " << px_h_hist << std::endl;
+	std::cout << "px_w_hist = " << px_w_hist << std::endl;
+	std::cout << "dropped_frames = " << dropped_frames << std::endl;
 
 	printf("Stopping acquisition...\n");
 	SAVECALL(xiStopAcquisition(xiH));
