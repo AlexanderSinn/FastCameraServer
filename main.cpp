8da925db1760760f955cf02d1a36a8c08e5a1dea
Alexander Sinn <alexander.sinn@desy.de>
2026-01-23 16:54:41 +0100

    add tile based center of mass calculation

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@
 #include <map>
 #include <iomanip>
 #include <cmath>
+#include <array>
 
 #ifdef min
 #undef min
@@ -54,6 +55,147 @@ std::ostream& operator<< (std::ostream& os, std::vector<T> const& vec) {
 	return os;
 }
 
+template<class T>
+void print_percent(std::vector<T> vec) {
+	T totsum = 0;
+	for (auto& v : vec) {
+		totsum += v;
+	}
+
+	std::cout << "fraction:";
+
+	for (double frac : std::array{0.5, 0.9, 0.99, 0.999}) {
+		T parsum = 0;
+		for (int i = 0; i < vec.size(); ++i) {
+			parsum += vec[i];
+			if (parsum >= static_cast<T>(totsum * frac)) {
+				std::cout << "\t" << i;
+				break;
+			}
+		}
+	}
+
+	std::cout << std::endl;
+}
+
+static
+void calc_moments(int im_height, int im_width, unsigned char * imager_ptr,
+				  std::vector<long long>& px_h_hist, std::vector<long long>& px_w_hist) {
+	float px_sum = 0;
+	float px_h_sum = 0;
+	float px_w_sum = 0;
+
+#ifdef _OPENMP
+#pragma omp parallel for reduction(+:px_sum,px_w_sum,px_h_sum)
+#endif
+	for (int j = 0; j < im_height; ++j) {
+		const float fj = static_cast<float>(j);
+		const auto ptr = imager_ptr + j * im_width;
+		float fi = 0;
+		for (int i = 0; i < im_width; ++i) {
+			float value = static_cast<float>(ptr[i]);
+			px_sum += value;
+			px_w_sum += value * fi;
+			px_h_sum += value * fj;
+
+			fi += 1;
+		}
+	}
+
+	px_h_hist[static_cast<int>(px_h_sum / px_sum)] += 1;
+	px_w_hist[static_cast<int>(px_w_sum / px_sum)] += 1;
+}
+
+static
+std::vector<int> make_tile_start(int nt, int imsize)
+{
+	std::vector<int> ret(nt + 1, 0);
+	for (int i = 0; i < nt + 1; ++i) {
+		ret[i] = (i * imsize) / nt;
+	}
+	return ret;
+}
+
+static
+void calc_tile(int nt_h, int nt_w, unsigned char* imager_ptr,
+	std::vector<int> const& tile_start_h, std::vector<int> const& tile_start_w,
+	std::vector<float>& tile_sum_h, std::vector<float>& tile_sum_w, int im_width)
+{
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+	for (int it = 0; it < nt_w*nt_h; ++it) {
+		int it_h = it / nt_w;
+		int it_w = it - nt_w * it_h;
+
+		float px_sum = 0;
+		float px_h_sum = 0;
+		float px_w_sum = 0;
+
+		int tbegin_h = tile_start_h[it_h];
+		int tbegin_w = tile_start_w[it_w];
+		int tend_h = tile_start_h[it_h + 1];
+		int tend_w = tile_start_w[it_w + 1];
+
+		for (int j = tbegin_h; j < tend_h; ++j) {
+			const float fj = static_cast<float>(j);
+			const auto ptr = imager_ptr + j * im_width;
+			float fi = static_cast<float>(tbegin_w);
+			for (int i = tbegin_w; i < tend_w; ++i) {
+				float value = static_cast<float>(ptr[i]);
+				px_sum += value;
+				px_w_sum += value * fi;
+				px_h_sum += value * fj;
+
+				fi += 1;
+			}
+		}
+
+		tile_sum_w[it] += px_w_sum / px_sum;
+		tile_sum_h[it] += px_h_sum / px_sum;
+	}
+}
+
+static
+void print_image(XI_IMG& image) {
+	std::cout << "size " << image.size << '\n';
+	std::cout << "bp " << image.bp << '\n';
+	std::cout << "bp_size " << image.bp_size << '\n';
+	std::cout << "frm " << image.frm << '\n';
+	std::cout << "width " << image.width << '\n';
+	std::cout << "height " << image.height << '\n';
+	std::cout << "nframe " << image.nframe << '\n';
+	std::cout << "tsSec " << image.tsSec << '\n';
+	std::cout << "tsUSec " << image.tsUSec << '\n';
+	std::cout << "GPI_level " << image.GPI_level << '\n';
+	std::cout << "black_level " << image.black_level << '\n';
+	std::cout << "padding_x " << image.padding_x << '\n';
+	std::cout << "AbsoluteOffsetX " << image.AbsoluteOffsetX << '\n';
+	std::cout << "AbsoluteOffsetY " << image.AbsoluteOffsetY << '\n';
+	std::cout << "transport_frm " << image.transport_frm << '\n';
+	std::cout << "gain_db " << image.gain_db << '\n';
+
+
+
+	float sum = 0.;
+
+	for (int j = 0; j != image.height; ++j) {
+		for (int i = 0; i != image.width; ++i) {
+			sum += static_cast<float>(((unsigned char*)image.bp)[i + j * image.width]);
+		}
+	}
+
+	sum /= image.height * image.width;
+
+	for (int j = 0; j != image.height; ++j) {
+		for (int i = 0; i != image.width; ++i) {
+			std::cout << (static_cast<float>(((unsigned char*)image.bp)[i + j * image.width]) > sum ? '#' : '-');
+		}
+		std::cout << '\n';
+	}
+	std::cout << '\n';
+}
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -75,6 +217,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	XI_IMG image;
 	memset(&image, 0, sizeof(image));
 	image.size = SIZE_XI_IMG_V2;
+	image.size = sizeof(image);
 
 	HANDLE xiH = NULL;
 
@@ -83,104 +226,80 @@ int _tmain(int argc, _TCHAR* argv[])
 	SAVECALL(xiOpenDevice(0, &xiH);)
 
 
-		for (auto& [pname, tname] : all_params) {
-			char result[512] = {};
-			XI_RETURN res = xiGetParamString(xiH, pname, result, 512);
-			std::cout << std::setw(5) << res << " " << std::setw(40) << tname << ": " << result << std::endl;
-		}
+	//for (auto& [pname, tname] : all_params) {
+	//	char result[512] = {};
+	//	XI_RETURN res = xiGetParamString(xiH, pname, result, 512);
+	//	std::cout << std::setw(5) << res << " " << std::setw(40) << tname << ": " << result << std::endl;
+	//}
 
-
-	// Setting "exposure" parameter (10ms=10000us)
-	SAVECALL(xiSetParamInt(xiH, XI_PRM_EXPOSURE, 10));
-
-
-	const int im_width = 608;
-	const int im_height = 608;
+	//const int im_width = 608;
+	//const int im_height = 608;
 	//const int im_width = 384;
 	//const int im_height = 384;
+	const int im_width = 608;
+	const int im_height = 608;
 
+	SAVECALL(xiSetParamInt(xiH, XI_PRM_DOWNSAMPLING, XI_DWN_2x2));
+	SAVECALL(xiSetParamInt(xiH, XI_PRM_DOWNSAMPLING_TYPE, XI_BINNING));
+
+	SAVECALL(xiSetParamInt(xiH, XI_PRM_EXPOSURE, 10));
 	SAVECALL(xiSetParamInt(xiH, XI_PRM_WIDTH, im_width));
 	SAVECALL(xiSetParamInt(xiH, XI_PRM_HEIGHT, im_height));
-
-	SAVECALL(xiSetParamInt(xiH, XI_PRM_GAIN, 1000000));
-
-	// set software trigger:
+	SAVECALL(xiSetParamFloat(xiH, XI_PRM_GAIN, 2.3F));
 	SAVECALL(xiSetParamInt(xiH, XI_PRM_TRG_SOURCE, XI_TRG_SOFTWARE));
-
-
-	// Note:
-	// The default parameters of each camera might be different in different API versions
-	// In order to ensure that your application will have camera in expected state,
-	// please set all parameters expected by your application to required value.
 
 	printf("Starting acquisition...\n");
 	SAVECALL(xiStartAcquisition(xiH));
 
 	SAVECALL(xiSetParamInt(xiH, XI_PRM_TRG_SOFTWARE, 1));
-	SAVECALL(xiGetImage(xiH, 5000, &image));
+	while (xiGetImage(xiH, 5000, &image) == XI_OK) {}
+	//SAVECALL(xiGetImage(xiH, 5000, &image));
 
-	//auto last = std::chrono::steady_clock::now();
 
+	//print_image(image);
+
+	int ntiles_h = 20;
+	int ntiles_w = 20;
+		
 	constexpr int expected_images = 100000;
 
 	long long last_image_num = 0;
 	long long dropped_frames = 0;
+	long long duplicate_frames = 0;
 
-	std::vector<long long> time_hist1(1000, 0);
-	std::vector<long long> time_hist2(1000, 0);
-	std::vector<long long> time_hist3(1000, 0);
-	std::vector<long long> time_hist4(1000, 0);
+	std::vector<long long> time_hist1(2000, 0);
+	std::vector<long long> time_hist2(2000, 0);
 
 	std::vector<long long> px_h_hist(image.height, 0);
 	std::vector<long long> px_w_hist(image.width, 0);
 
-	constexpr bool do_moments = true;
+	auto tile_start_h = make_tile_start(ntiles_h, image.height);
+	auto tile_start_w = make_tile_start(ntiles_w, image.width);
+
+	std::vector<float> tile_sum_h(ntiles_h * ntiles_w, 0.F);
+	std::vector<float> tile_sum_w(ntiles_h * ntiles_w, 0.F);
+
+	constexpr bool do_moments = false;
+	constexpr bool do_tiles = true;
 
 	for (int images = 0; images < expected_images; images++)
 	{
 		auto t1 = std::chrono::steady_clock::now();
 
 		SAVECALL(xiSetParamInt(xiH, XI_PRM_TRG_SOFTWARE, 1));
+		SAVECALL(xiGetImage(xiH, 5000, &image));
 
 		auto t2 = std::chrono::steady_clock::now();
 
-		SAVECALL(xiGetImage(xiH, 5000, &image));
-
-		auto t3 = std::chrono::steady_clock::now();
-
 		if constexpr (do_moments) {
-			float px_sum = 0;
-			float px_h_sum = 0;
-			float px_w_sum = 0;
-
-#ifdef _OPENMP
-#pragma omp parallel for reduction(+:px_sum,px_w_sum,px_h_sum)
-#endif
-			for (int j = 0; j < im_height; ++j) {
-				const float fj = static_cast<float>(j);
-				const auto ptr = (unsigned char*)image.bp + j * im_width;
-				float fi = 0;
-				for (int i = 0; i < im_width; ++i) {
-					float value = static_cast<float>(ptr[i]);
-					px_sum += value;
-					px_w_sum += value * fi;
-					px_h_sum += value * fj;
-
-					fi += 1;
-				}
-			}
-
-			px_h_hist[static_cast<int>(px_h_sum / px_sum)] += 1;
-			px_w_hist[static_cast<int>(px_w_sum / px_sum)] += 1;
-
-			//printf("h %lld, w %lld\n", px_h_sum / px_sum, px_w_sum / px_sum);
+			calc_moments(im_height, im_width, (unsigned char*)image.bp, px_h_hist, px_w_hist);
 		}
 
-		auto t4 = std::chrono::steady_clock::now();
+		if constexpr (do_tiles) {
+			calc_tile(ntiles_h, ntiles_w, (unsigned char*)image.bp, tile_start_h, tile_start_w, tile_sum_w, tile_sum_h, im_width);
+		}
 
-		//auto current_time = std::chrono::steady_clock::now();
-		//auto delta = std::chrono::duration<double, std::micro>(current_time - last);
-		//last = current_time;
+		auto t3 = std::chrono::steady_clock::now();
 
 		{
 			auto delta = std::chrono::duration<double, std::micro>(t2 - t1);
@@ -188,47 +307,35 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 
 		{
-			auto delta = std::chrono::duration<double, std::micro>(t3 - t2);
+			auto delta = std::chrono::duration<double, std::micro>(t3 - t1);
 			time_hist2[std::max(std::min(static_cast<int>(delta.count()), static_cast<int>(time_hist2.size() - 1)), 0)] += 1;
 		}
 
-		{
-			auto delta = std::chrono::duration<double, std::micro>(t4 - t3);
-			time_hist3[std::max(std::min(static_cast<int>(delta.count()), static_cast<int>(time_hist3.size() - 1)), 0)] += 1;
-		}
-
-		{
-			auto delta = std::chrono::duration<double, std::micro>(t4 - t1);
-			time_hist4[std::max(std::min(static_cast<int>(delta.count()), static_cast<int>(time_hist4.size() - 1)), 0)] += 1;
-		}
-
-		
-
 		if (images > 0) {
 			dropped_frames += std::max<long long>((image.nframe - last_image_num - 1), 0);
+			duplicate_frames += std::max<long long>(-(image.nframe - last_image_num - 1), 0);
 		}
 		last_image_num = image.nframe;
 
-		//printf("%f us, ptr %p nframe %d, tsSec %d, tsUSec %d  \n", delta.count(), image.bp, image.nframe, image.tsSec, image.tsUSec);
-
-		while (true) {
-			auto t5 = std::chrono::steady_clock::now();
-			auto delta = std::chrono::duration<double, std::micro>(t5 - t1);
-			if (delta.count() >= 1000.) {
-				break;
-			}
-		}
 	}
 
 	std::cout << "time_hist1 = " << time_hist1 << std::endl;
+	print_percent(time_hist1);
 	std::cout << "time_hist2 = " << time_hist2 << std::endl;
-	std::cout << "time_hist3 = " << time_hist3 << std::endl;
-	std::cout << "time_hist4 = " << time_hist4 << std::endl;
+	print_percent(time_hist2);
+
 	if constexpr (do_moments) {
 		std::cout << "px_h_hist = " << px_h_hist << std::endl;
 		std::cout << "px_w_hist = " << px_w_hist << std::endl;
 	}
+
+	if constexpr (do_tiles) {
+		std::cout << "tile_sum_h = " << tile_sum_h << std::endl;
+		std::cout << "tile_sum_w = " << tile_sum_w << std::endl;
+	}
+
 	std::cout << "dropped_frames = " << dropped_frames << std::endl;
+	std::cout << "duplicate_frames = " << duplicate_frames << std::endl;
 
 	printf("Stopping acquisition...\n");
 	SAVECALL(xiStopAcquisition(xiH));
